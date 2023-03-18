216c5c9f2638df5abc60dfeab3eec6bc88224b56
Naoya Yamashita <conao3@gmail.com>
2023-03-18 11:33:29 +0900

    use pydantic to load .env

--- src/sqlalchemy_bindparam/main.py
+++ src/sqlalchemy_bindparam/main.py
@@ -1,4 +1,24 @@
+import pydantic
+
+
+### load settings from .env file
+
+class Settings(pydantic.BaseSettings):
+    db_name: str
+    db_user: str
+    db_password: str
+    db_port: int
+
+    class Config:  # type: ignore
+        env_file = ".env"
+
+
+settings = Settings()  # type: ignore
+
+
 def main1():
     print("Hello World!")
+    print(settings)
+
 
 main1()
